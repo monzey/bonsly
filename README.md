6f0621c7900a20dbba2ce9cc9e225f8214884fda
Maxime Bertrand <maxi.bertrand@gmail.com>
2018-06-01 19:10:37 +0200

    added readme and install script

---
+++ README.md
@@ -0,0 +1,16 @@
+# Max's dotfiles
+
+Currently supports the following confs:
++ alacritty
++ feh
++ i3
++ ncmpcpp
++ polybar
++ qutebrowser
++ st
++ tig
++ tmux
++ vim
++ zsh
+
+run ``./install.sh`` to install symlinks in your $HOME directory
---
+++ install.sh
@@ -0,0 +1,15 @@
+dir=$(pwd -P)
+
+ln -sf $dir/.tigrc $HOME/.tigrc
+ln -sf $dir/.tmux.conf $HOME/.config/.tmux.conf
+ln -sf $dir/.vim $HOME
+ln -sf $dir/.vimrc $HOME/.vimrc
+ln -sf $dir/.zshrc $HOME/.zshrc
+ln -sf $dir/.zprofile $HOME/.zprofile
+ln -sf $dir/.xinitrc $HOME/.xinitrc
+ln -sf $dir/compton.conf $HOME/.config/compton.conf
+ln -sf $dir/feh $HOME/.config/
+ln -sf $dir/qutebrowser $HOME/.config/
+ln -sf $dir/polybar $HOME/.config/
+ln -sf $dir/i3 $HOME/.config/
+ln -sf $dir/.ncmpcpp $HOME/.ncmpcpp
