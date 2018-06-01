894e86957175d9faff2c041cf5e914f129647b0b
Maxime Bertrand <maxi.bertrand@gmail.com>
2018-06-01 11:29:04 +0200

    activated blockedhosts for adblock for qutebrowser

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -540,14 +540,14 @@ c.completion.shrink = True
 
 ## Enable host blocking.
 ## Type: Bool
-# c.content.host_blocking.enabled = True
+c.content.host_blocking.enabled = True
 
 ## List of URLs of lists which contain hosts to block.  The file can be
 ## in one of the following formats:  - An `/etc/hosts`-like file - One
 ## host per line - A zip-file of any of the above, with either only one
 ## file, or a file   named `hosts` (with any extension).
 ## Type: List of Url
-# c.content.host_blocking.lists = ['https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts']
+c.content.host_blocking.lists = ['https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts', str(config.configdir) + "/blockedHosts"]
 
 ## List of domains that should always be loaded, despite being ad-
 ## blocked. Domains may contain * and ? wildcards and are otherwise
