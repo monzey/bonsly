e8957762874bd507b35addb427b5413ce76fa274
root <root@debian>
2018-11-09 11:45:39 +0000

    revamped dotfiles to be managed by stow & removed atom conf

--- install.sh
+++ install.sh
@@ -1,21 +1,29 @@
-dir=$(pwd -P)
+stow ranger
+stow vim
+stow alacritty
+stow x
+stow tmux
+stow feh
+stow polybar
+stow qutebrowser
 
-ln -sf $dir/alacritty $HOME/.config/
-ln -sf $dir/.tigrc $HOME/.tigrc
-ln -sf $dir/.tmux.conf $HOME/.config/.tmux.conf
-ln -sf $dir/.vim $HOME
-ln -sf $dir/.vimrc $HOME/.vimrc
-ln -sf $dir/.zshrc $HOME/.zshrc
-ln -sf $dir/.zprofile $HOME/.zprofile
-ln -sf $dir/.xinitrc $HOME/.xinitrc
-ln -sf $dir/compton.conf $HOME/.config/compton.conf
-ln -sf $dir/feh $HOME/.config/
-ln -sf $dir/qutebrowser $HOME/.config/
-ln -sf $dir/polybar $HOME/.config/
-ln -sf $dir/i3 $HOME/.config/
-ln -sf $dir/.ncmpcpp $HOME
-ln -sf $dir/rofi $HOME/.config
-ln -sf $dir/git/.gitignore $HOME/.gitignore
-ln -sf $dir/git/.gitconfig $HOME/.gitconfig
-ln -sf $dir/ranger/rc.conf $HOME/.config/ranger/rc.conf
-ln -sf $dir/.Xmodmap $HOME/.Xmodmap
+# ln -sf $dir/alacritty $HOME/.config/
+# ln -sf $dir/.tigrc $HOME/.tigrc
+# ln -sf $dir/.tmux.conf $HOME/.config/.tmux.conf
+# ln -sf $dir/.vim $HOME
+# ln -sf $dir/.vimrc $HOME/.vimrc
+# ln -sf $dir/.zshrc $HOME/.zshrc
+# ln -sf $dir/.zprofile $HOME/.zprofile
+# ln -sf $dir/.xinitrc $HOME/.xinitrc
+# ln -sf $dir/compton.conf $HOME/.config/compton.conf
+# ln -sf $dir/feh $HOME/.config/
+# ln -sf $dir/qutebrowser $HOME/.config/
+# ln -sf $dir/polybar $HOME/.config/
+# ln -sf $dir/i3 $HOME/.config/
+# ln -sf $dir/.ncmpcpp $HOME
+# ln -sf $dir/rofi $HOME/.config
+# ln -sf $dir/git/.gitignore $HOME/.gitignore
+# ln -sf $dir/git/.gitconfig $HOME/.gitconfig
+# ln -sf $dir/ranger/rc.conf $HOME/.config/ranger/rc.conf
+# ln -sf $dir/.Xmodmap $HOME/.Xmodmap
+# ln -sf $dir/.tmux.conf $HOME/.tmux.conf
