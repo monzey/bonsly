ebcb9b841a53865e0de87a34b1e38f8f3aa10777
Maxime Bertrand <maxi.bertrand@gmail.com>
2018-11-13 23:49:27 +0100

    No need nodm anymore

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 apt install -y curl build-essential cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev
-apt install -y chromium stterm stow nodm xfonts-base xserver-xorg-input-all xinit xserver-xorg xserver-xorg-video-all cargo ranger zsh zsh-antigen tig feh vim tmux rofi compton openssh-server git i3 suckless-tools 
+apt install -y chromium stterm stow xfonts-base xserver-xorg-input-all xinit xserver-xorg xserver-xorg-video-all cargo ranger zsh zsh-antigen tig feh vim tmux rofi compton openssh-server git i3 suckless-tools 
 
 # install i3 gaps
 curl -sL https://raw.githubusercontent.com/maestrogerardo/i3-gaps-deb/master/i3-gaps-deb | bash -
