071928587f6f044a7774207d6bb90322322d9285
root <root@debian>
2018-11-22 16:02:18 +0000

    added urxvt 256 color & exported vim as default editor

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 dir=$(pwd)
 
 apt install -y curl build-essential cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev sudo
-apt install -y chromium rxvt-unicode stow xfonts-base xserver-xorg-input-all xinit xserver-xorg xserver-xorg-video-all cargo ranger zsh zsh-antigen tig feh vim tmux rofi compton openssh-server git i3 suckless-tools 
+apt install -y chromium rxvt-unicode rxvt-unicode-256color stow xfonts-base xserver-xorg-input-all xinit xserver-xorg xserver-xorg-video-all cargo ranger zsh zsh-antigen tig feh vim tmux rofi compton openssh-server git i3 suckless-tools 
 
 # install i3 gaps
 curl -sL https://raw.githubusercontent.com/maestrogerardo/i3-gaps-deb/master/i3-gaps-deb | bash -
@@ -26,6 +26,3 @@ curl -L git.io/antigen > /usr/bin/antigen
 # install fonts
 cp $dir/fonts/* /usr/share/fonts
 fc-cache -fv
-
-# Vim as default editor
-export EDITOR=vim
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -27,7 +27,10 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-export TERM=xterm-256color
+
+# Vim as default editor
+export EDITOR=vim
+export TERM=rxvt-unicode-256color
 export PATH=/usr/lib/dart/bin:$PATH
 export PATH=$PATH:~/.pub-cache/bin
 # Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
