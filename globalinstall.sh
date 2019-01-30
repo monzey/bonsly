72e5e7f188d274abdcc2858962b6a06f2e138d69
Maxime Bertrand <maxi.bertrand@gmail.com>
2019-01-30 15:15:54 +0100

    Added missing packages

--- globalinstall.sh
+++ globalinstall.sh
@@ -34,7 +34,7 @@ installCustomEnvironment () {
 
     apt install -y libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-composite0-dev sudo
     apt install -y chromium stow xfonts-base xserver-xorg-input-all xinit xserver-xorg xserver-xorg-video-all cargo ranger zsh zsh-antigen tig feh vim-nox tmux rofi compton i3 suckless-tools xclip silversearcher-ag nodejs
-    apt install -y dbus-x11 libdbus-1-dev libx11-dev libxinerama-dev libxrandr-dev libxss-dev libglib2.0-dev libpango1.0-dev libgtk-3-dev libxdg-basedir-dev dirmngr ffmpeg
+    apt install -y dbus-x11 libdbus-1-dev libx11-dev libxinerama-dev libxrandr-dev libxss-dev libglib2.0-dev libpango1.0-dev libgtk-3-dev libxdg-basedir-dev dirmngr ffmpeg x11-apps
 
     npm install -g joplin
 
@@ -53,6 +53,9 @@ installCustomEnvironment () {
 
     wget http://ftp.fr.debian.org/debian/pool/main/q/qutebrowser/qutebrowser-qtwebengine_1.5.2-1_all.deb -O /tmp/qtwebengine.deb
     dpkg -i /tmp/qtwebengine.deb
+
+    wget https://github.com/advanced-rest-client/arc-electron/releases/download/v12.1.4/arc-linux-12.1.4-amd64.deb -O /tmp/arc.deb
+    dpkg -i /tmp/arc.deb
 
     apt install -y --fix-broken
 
