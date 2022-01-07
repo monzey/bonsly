5d621002ceee7201a436b9f019110a53064526a9
Maxime Bertrand <maxi.bertrand@gmail.com>
2022-01-07 16:40:09 +0100

    fix regolith ppa

--- init.sh
+++ init.sh
@@ -1,10 +1,11 @@
 #! /bin/bash 
 
 installAnsible () {
+  apt install curl gnupg -y
   echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu focal main" | tee -a /etc/apt/sources.list
   curl -sL "http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x93C4A3FD7BB9C367" | apt-key add
   apt update
-  apt install ansible
+  apt install ansible -y
 
   # launch playbook
   ansible-playbook playbook.yaml --ask-become-pass -vvv
