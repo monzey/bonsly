7802152d1b25de0bd72b80b62b3dd42b5b1c348e
Maxime Bertrand <maxi.bertrand@gmail.com>
2022-01-07 10:37:51 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,11 +1,11 @@
 #! /bin/bash 
 
 installAnsible () {
-  deb http://ppa.launchpad.net/ansible/ansible/ubuntu focal main
-  sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-  sudo apt update
-  sudo apt install ansible
+  echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu focal main" | tee -a /etc/apt/sources.list
+  apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+  apt update
+  apt install ansible
 
   # launch playbook
-  ansible-playbook playbook.yaml --ask-become-pass
+  ansible-playbook playbook.yaml --ask-become-pass -vvv
 }
