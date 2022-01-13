8512e29d21d4455cc7d5cae9e1f01279807bd82b
Maxime Bertrand <maxi.bertrand@gmail.com>
2022-01-13 16:37:25 +0100

    rofi updates & install brew

--- install.sh
+++ install.sh
@@ -42,4 +42,7 @@ ln -sf $pwd/.fonts.conf ~
 # google-chrome as default web browser 
 xdg-settings set default-web-browser google-chrome.desktop
 
+# alacritty as default terminal
+sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/bin/alacritty 60
+
 cd ~
