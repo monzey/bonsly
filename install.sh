bdc64f184d45495640e0da1b42c464730541adab
Maxime Bertrand <maxi.bertrand@gmail.com>
2024-09-20 14:12:38 +0200

    minimal working nixos config

--- install.sh
+++ install.sh
@@ -1,40 +1,44 @@
 #! /bin/bash 
 
-pwd=`pwd`
-
+sudo nixos-generate-config
 nix-shell -p stow
 
-stow bat
-stow dunst
-stow feh
-stow git
-stow ranger
-stow rofi
-stow nvim
-stow zsh
-stow lazygit
+pwd=`pwd`
+
+# stow bat
+# stow dunst
+# stow feh
+# stow git
+# stow ranger
+# stow rofi
+# stow nvim
+# stow zsh
+# stow lazygit
 stow kitty
-stow lsd
-stow btop
-stow scripts -t /usr
-stow nix -t /etc
-
-chsh -s $(which zsh)
-
-mkdir -p ~/.icons/default
-cp -r cursor/* ~/.icons/default
-
-ln -sf $pwd/.fonts.conf ~
-
-# firefox as default web browser 
-xdg-settings set default-web-browser firefox-esr.desktop
-
-# copy wallpaper
-cp wall.png ~/
-
-# kitty as default terminal
-sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/bin/kitty 60
+# stow lsd
+# stow btop
+# stow scripts -t /usr
+stow nixos -t /etc
+stow hyprland -t /etc
 
 sudo nixos-rebuild switch
 
-cd ~
+# chsh -s $(which zsh)
+#
+# mkdir -p ~/.icons/default
+# cp -r cursor/* ~/.icons/default
+#
+# ln -sf $pwd/.fonts.conf ~
+#
+# # firefox as default web browser 
+# xdg-settings set default-web-browser firefox-esr.desktop
+#
+# # copy wallpaper
+# cp wall.png ~/
+#
+# # kitty as default terminal
+# sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/bin/kitty 60
+#
+# sudo nixos-rebuild switch
+#
+# cd ~
