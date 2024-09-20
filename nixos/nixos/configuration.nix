635ad827fdcc6d15ffc0ca60986aedd35602808b
Maxime Bertrand <maxi.bertrand@gmail.com>
2024-09-20 19:50:26 +0200

    default hyprland config

--- nixos/nixos/configuration.nix
+++ nixos/nixos/configuration.nix
@@ -1,66 +1,63 @@
 { config, pkgs, ... }:
 
 {
-  # Paramètres de base de NixOS
   imports = [ 
     /etc/nixos/hardware-configuration.nix
   ];
 
-  # Nom de la machine
-  system.stateVersion = "23.05";  # Version de NixOS à ajuster selon votre version
+  system.stateVersion = "24.05";  # Version de NixOS à ajuster selon votre version
 
   # Configurations basiques du système
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "muk";
+  networking.wireless.enable = true;
+  networking.networkmanager.enable = true;
+
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  time.timeZone = "Europe/Paris";
+
+  i18n.defaultLocale = "fr_FR.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "fr_FR.UTF-8";
+    LS_IDENTIFICATION = "fr_FR.UTF-8";
+    LS_MEASUREMENT = "fr_FR.UTF-8";
+    LS_MONETARY = "fr_FR.UTF-8";
+    LS_NAME = "fr_FR.UTF-8";
+    LS_NUMERIC = "fr_FR.UTF-8";
+    LS_PAPER = "fr_FR.UTF-8";
+    LS_TELEPHONE = "fr_FR.UTF-8";
+    LS_TIME = "fr_FR.UTF-8";
+  } 
+
+  console.keyMap = "fr";
+
+  user.users.monzey = {
+    isNormalUser = true;
+    description = "monzey";
+    extraGroups = [ "wheel" "networkmanager" ];
+  }
+
+  services.dbus.enable = true;   # Nécessaire pour le fonctionnement de nombreux composants sous Wayland
+  services.udev.packages = [ pkgs.libinput ];  # Nécessaire pour les périphériques d'entrée sous Wayland
+
+  boot.kernelModules = [ "kvm-intel" ];  # Pour Intel, remplacez par "kvm-amd" si vous avez un processeur AMD
+
+  nixpkgs.config.allowUnfree = true;
 
   programs.hyprland.enable = true; # enable Hyprland
 
   # Optional, hint Electron apps to use Wayland:
   # environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
-  # Configuration Wayland et Hyprland
-  # services.wayland = {
-  #   enable = true;
-  #
-  #   # Hyprland - Compositeur Wayland
-  #   compositor = {
-  #     hyprland.enable = true;
-  #     hyprland.configFile = "/etc/hypr/hyprland.conf";  # Vous pouvez personnaliser le fichier de configuration
-  #   };
-  # };
-
-  # Gestionnaire de login pour Wayland (Greetd)
-  # services.greetd = {
-  #   enable = true;
-  #   defaultSession = "Hyprland";
-  # };
-
-  # Notifications avec Mako
-  # services.mako = {
-  #   enable = true;
-  # };
-
   # Installation de logiciels essentiels
   environment.systemPackages = with pkgs; [
     # neovide      
     # mako         
-    # hyprland     
     kitty
     # waybar       
-    networkmanager  
   ];
-
-  # Configurations réseau
-  networking.networkmanager.enable = true;
-
-  # Autres services nécessaires
-  services.dbus.enable = true;   # Nécessaire pour le fonctionnement de nombreux composants sous Wayland
-  services.udev.packages = [ pkgs.libinput ];  # Nécessaire pour les périphériques d'entrée sous Wayland
-
-  # Gestion du son
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-
-  # Configuration du noyau
-  boot.kernelModules = [ "kvm-intel" ];  # Pour Intel, remplacez par "kvm-amd" si vous avez un processeur AMD
 }
