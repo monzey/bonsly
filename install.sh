84f12520fbb2f1e240ffdd6af382aabc27887144
Maxime Bertrand <maxi.bertrand@gmail.com>
2024-09-22 20:14:25 +0200

    configure regreet & misc

--- install.sh
+++ install.sh
@@ -6,21 +6,21 @@ sudo rm -rf /etc/nixos/configuration.nix
 sudo rm -rf /etc/hypr/hyprland.conf
 
 sudo stow bat
-# stow dunst
-# stow git
-# stow ranger
+sudo stow dunst
+sudo stow git
+sudo stow ranger
 sudo stow wofi
-sudo stow greetd
+sudo stow regreet
 sudo stow nvim
 sudo stow zsh
 sudo stow lazygit
 sudo stow kitty
 sudo stow btop
-# stow scripts -t /usr
+sudo stow scripts -t /usr
 sudo stow nixos -t /etc
 sudo stow hyprland
 
-curl -L git.io/antigen > $HOME/.antigen.zsh
+# curl -L git.io/antigen > $HOME/.antigen.zsh
 
 chsh -s $(which zsh)
 #
@@ -29,5 +29,7 @@ chsh -s $(which zsh)
 #
 # copy wallpaper
 cp wall.png ~/
+sudo mkdir -p /usr/share/backgrounds/
+sudo cp wall.png /usr/share/backgrounds/greeter
 
 sudo nixos-rebuild switch
