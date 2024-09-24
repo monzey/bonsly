704fb63f7af628be584c0ab313d3650c6fae0474
Maxime Bertrand <maxi.bertrand@gmail.com>
2024-09-24 16:03:41 +0200

    add nvim conf

---
+++ nvim/.config/nvim/lua/plugins/mappings.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = {
+      mappings = {
+        n = {
+          ["<A-j>"] = { "<C-w>h", desc = "Move to left window" },
+          ["<A-m>"] = { "<C-w>l", desc = "Move to right window" },
+          ["<A-k>"] = { "<C-w>j", desc = "Move to down window" },
+          ["<A-l>"] = { "<C-w>k", desc = "Move to up window" },
+          ["<C-K>"] = { "<cmd>bnext<cr>", desc = "Move to next buffer" },
+          ["<C-J>"] = { "<cmd>bprevious<cr>", desc = "Move to previous buffer" },
+        },
+      },
+    },
+  },
+}
