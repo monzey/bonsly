45edaeb41a306f21b8465fef4495e8b3e0b51020
Maxime Bertrand <maxi.bertrand@gmail.com>
2025-03-27 05:55:00 +0100

    chore: re add docker

---
+++ home-manager/configs/xplr/init.lua
@@ -0,0 +1,8 @@
+version = '0.21.9'
+local home = os.getenv("HOME")
+package.path = home
+.. "/.config/xplr/plugins/?/init.lua;"
+.. home
+.. "/.config/xplr/plugins/?.lua;"
+.. package.path
+require"icons".setup()
