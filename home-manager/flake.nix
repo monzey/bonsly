5227acb5e79575c64fd0be5cabd600d457b41dd3
Maxime Bertrand <maxi.bertrand@gmail.com>
2025-04-04 16:50:42 +0200

    feat: add hyprpanel setup

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -4,21 +4,30 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     nixos-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    hyprpanel.url = "github:Jas-SinghFSU/HyprPanel";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, home-manager, nixos-unstable, ... }@inputs: 
+  outputs = { nixpkgs, home-manager, nixos-unstable, hyprpanel, ... }@inputs: 
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      unstablePkgs = nixos-unstable.legacyPackages.${system};
+      # pkgs = nixpkgs.legacyPackages.${system};
+      # unstablePkgs = nixos-unstable.legacyPackages.${system};
     in {
       homeConfigurations."monzey" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;  
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            inputs.hyprpanel.overlay
+          ];
+        };
+
         extraSpecialArgs = {
+          inherit system;
+          inherit inputs;
           unstablePkgs = import nixos-unstable {
             inherit system;
             config.allowUnfree = true;
