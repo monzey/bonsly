406b0ccf1b2e9596e33b8ab9c194234f352fd427
Maxime Bertrand <maxi.bertrand@gmail.com>
2025-07-04 17:31:31 +0200

    clean checkpoint

--- home-manager/configs/nvim/lua/plugins/avante.lua
+++ home-manager/configs/nvim/lua/plugins/avante.lua
@@ -5,7 +5,7 @@ return {
       provider = "copilot",
       providers = {
         copilot = {
-          model = "claude-3.5-sonnet",
+          model = "claude-3.7-sonnet",
         },
       },
       windows = {
